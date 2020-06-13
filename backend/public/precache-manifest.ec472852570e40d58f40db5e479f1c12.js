7fed00c2c713cb690f874f1a25c18a0da7f48a0b
talmashiah <talmashiah@gmail.com>
2020-06-14 02:39:49 +0300

    first deploy

---
+++ backend/public/precache-manifest.ec472852570e40d58f40db5e479f1c12.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcad693a974aa6cd0ce552562fc3b091",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b278777ab9517fad9a8d",
+    "url": "/static/css/2.e52e11b8.chunk.css"
+  },
+  {
+    "revision": "2c0f97f18e77f177b7f3",
+    "url": "/static/css/main.60cd3f91.chunk.css"
+  },
+  {
+    "revision": "b278777ab9517fad9a8d",
+    "url": "/static/js/2.8a93b5ee.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.8a93b5ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c0f97f18e77f177b7f3",
+    "url": "/static/js/main.2b881a54.chunk.js"
+  },
+  {
+    "revision": "cd5cc0b019c8434d78aa",
+    "url": "/static/js/runtime-main.95a77404.js"
+  },
+  {
+    "revision": "27a6a1f1070e624518060e9dc8fa9e53",
+    "url": "/static/media/OpenSansHebrew-Bold.27a6a1f1.ttf"
+  },
+  {
+    "revision": "612a355682445e4301c8186044d6c1e7",
+    "url": "/static/media/OpenSansHebrew-ExtraBold.612a3556.ttf"
+  },
+  {
+    "revision": "cfc63440fcf46210a7162420ad65159a",
+    "url": "/static/media/OpenSansHebrew-Light.cfc63440.ttf"
+  },
+  {
+    "revision": "11544c36c3b76127e8d84e894b4f1230",
+    "url": "/static/media/OpenSansHebrew-Regular.11544c36.ttf"
+  },
+  {
+    "revision": "8df8dd20ac9285d41bfbf0b5a99252f1",
+    "url": "/static/media/spinner.8df8dd20.png"
+  }
+]);
