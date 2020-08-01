c6e3fa6dcd09f3e070135be7bd4ee7d2a72aa7b7
talmashiah <talmashiah@gmail.com>
2020-08-01 23:51:49 +0300

    NavBar contact link

--- frontend/src/cmps/header/NavBar.jsx
+++ frontend/src/cmps/header/NavBar.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 import NavCart from '../cart/NavCart.jsx';
 import NavUser from '../user/NavUser.jsx';
 
 export default function NavBar({ bag, loggedInUser, deleteItem, changeQuantity, logout }) {
     return (
         <nav className='flex'>
+            <div className='link-container contact'>
+                <Link to="/contact"><i className="fas fa-phone"></i></Link>
+            </div>
+
             <NavUser loggedInUser={loggedInUser} logout={logout} />
             <NavCart bag={bag} deleteItem={deleteItem} changeQuantity={changeQuantity} />
         </nav>
