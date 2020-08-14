1e052541866aa93f881819bae68891e01c6e5b23
talmashiah <talmashiah@gmail.com>
2020-08-14 19:38:21 +0300

    search header turn to hooks

--- frontend/src/cmps/search/SearchHeader.jsx
+++ frontend/src/cmps/search/SearchHeader.jsx
@@ -1,50 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-// import SearchResultInfo from './SearchResultInfo';
 import SelectBox from '../general/SelectBox.jsx';
 
-export default class SearchHeader extends Component {
-    state = {
-        options: [
-            { key: 'bestMatch', value: 'רלוונטיות' },
-            { key: 'PriceAscending', value: 'מחיר: מהנמוך לגבוה' },
-            { key: 'PriceDescending', value: 'מחיר: מהגבוה לנמוך' }
-        ],
-        isSticky: false
-    };
-    title = React.createRef()
+export default function SearchHeader({ term, productsLength, updateSort, toggleFilters }) {
 
-    componentDidMount() {
-        window.addEventListener('scroll', this.handleScroll);
-    }
+    const [isSticky, setIsSticky] = useState(false);
+    const [options] = useState([
+        { key: 'bestMatch', value: 'רלוונטיות' },
+        { key: 'PriceAscending', value: 'מחיר: מהנמוך לגבוה' },
+        { key: 'PriceDescending', value: 'מחיר: מהגבוה לנמוך' }
+    ]);
+    
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, [])
+    
+    const title = useRef(null)
 
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.handleScroll);
-    }
-
-    handleScroll = () => {
-        if (window.scrollY > this.title.current.offsetHeight) {
-            this.setState({ isSticky: true });
+    const handleScroll = () => {
+        if (window.scrollY > title.current.offsetHeight) {
+            setIsSticky(true);
         } else {
-            this.setState({ isSticky: false });
+            setIsSticky(false);
         }
     }
 
-    render() {
-        const { term, productsLength, updateSort, toggleFilters } = this.props;
-        const { options, isSticky } = this.state;
+    return (
+        <div className="search-header flex align-center justify-between">
+            <h2 ref={title}>{productsLength === 0 ? 'לא מצאנו שום' : productsLength === 1 ? '' : productsLength} {productsLength === 1 ? 'תוצאה אחת' : 'תוצאות'} עבור <span>"{term}"</span> </h2>
+            {<div className={`btns-container flex ${isSticky ? 'sticky' : ''}`}>
+                <div className="filter-btn flex justify-center align-center" onClick={toggleFilters}>
+                    <i className="fas fa-filter"></i>
+                    <div className="title">סנן תוצאות</div>
+                </div>
+                {<SelectBox handleChange={updateSort} options={options} />}
+            </div>}
+        </div>
+    )
 
-        return (
-            <div className="search-header flex align-center justify-between">
-                <h2 ref={this.title}>{productsLength === 0 ? 'לא מצאנו שום' : productsLength === 1 ? '' : productsLength} {productsLength === 1 ? 'תוצאה אחת' : 'תוצאות'} עבור <span>"{term}"</span> </h2>
-                {<div className={`btns-container flex ${isSticky ? 'sticky' : ''}`}>
-                    <div className="filter-btn flex justify-center align-center" onClick={toggleFilters}>
-                        <i className="fas fa-filter"></i>
-                        <div className="title">סנן תוצאות</div>
-                    </div>
-                    {<SelectBox handleChange={updateSort} options={options} />}
-                </div>}
-            </div>
-        )
-    }
 }
