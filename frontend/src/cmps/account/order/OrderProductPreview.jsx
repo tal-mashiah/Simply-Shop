ba6b8072cc36484055f2ce1740ba0a247009d67b
talmashiah <talmashiah@gmail.com>
2020-08-18 02:16:07 +0300

    OrderPreview and OrderProductPreview turns to hooks

--- frontend/src/cmps/account/order/OrderProductPreview.jsx
+++ frontend/src/cmps/account/order/OrderProductPreview.jsx
@@ -1,20 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default class OrderProductPreview extends Component {
-    render() {
-        const { quantity, title, price, imagesUrl, productId } = this.props.product
-        return (
-            <div className="order-product-preview flex">
-                <Link to={`/product/${productId}`}><img src={imagesUrl[0]} alt="product img" /></Link>
-                <div className="info flex column justify-between">
-                    <div>
-                        <div className="title"><Link to={`/product/${productId}`}>{title}</Link></div>
-                        <div className="quantity">כמות: {quantity}</div>
-                    </div>
-                    <div className="price">{price}<i className="fas fa-shekel-sign"></i></div>
+export default function OrderProductPreview({ product }) {
+    
+    const { quantity, title, price, imagesUrl, productId } = product
+    return (
+        <div className="order-product-preview flex">
+            <Link to={`/product/${productId}`}><img src={imagesUrl[0]} alt="product img" /></Link>
+            <div className="info flex column justify-between">
+                <div>
+                    <div className="title"><Link to={`/product/${productId}`}>{title}</Link></div>
+                    <div className="quantity">כמות: {quantity}</div>
                 </div>
+                <div className="price">{price}<i className="fas fa-shekel-sign"></i></div>
             </div>
-        )
-    }
+        </div>
+    )
 }
