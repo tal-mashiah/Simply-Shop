bc71ad15c54e14a9787adfaa099d0314b7274143
talmashiah <talmashiah@gmail.com>
2020-08-18 03:09:56 +0300

    SearchProductPreview  turn to functional component

--- frontend/src/cmps/search-bar/SearchProductPreview.jsx
+++ frontend/src/cmps/search-bar/SearchProductPreview.jsx
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default class SearchProductPreview extends Component {
+export default function SearchProductPreview({ product, onProductClick }) {
 
-    render() {
-        const { imagesUrl, title, _id } = this.props.product;
-        return (
-            <Link to={`/product/${_id}`} onClick={() => this.props.onProductClick()}>
-                <div className="search-product-preview flex align-center">
-                    <div className="image-container">
-                        <img src={imagesUrl[0].thumbnail} alt={title} />
-                    </div>
-                    <div className="title">{title}</div>
+    const { imagesUrl, title, _id } = product;
+    return (
+        <Link to={`/product/${_id}`} onClick={() => onProductClick()}>
+            <div className="search-product-preview flex align-center">
+                <div className="image-container">
+                    <img src={imagesUrl[0].thumbnail} alt={title} />
                 </div>
-            </Link>
-        )
-    }
+                <div className="title">{title}</div>
+            </div>
+        </Link>
+    )
 }
