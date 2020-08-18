c78edf7be4ae15932d16a8bf28182e7c03cfaf88
talmashiah <talmashiah@gmail.com>
2020-08-18 03:35:43 +0300

    QuntityBar turn to functional component

--- frontend/src/cmps/general/QuntityBar.jsx
+++ frontend/src/cmps/general/QuntityBar.jsx
@@ -1,20 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-export default class QuntityBar extends Component {
+export default function QuntityBar({ changeQuantity, itemId, quantity }) {
 
-    handleChange = (diff) => {
-        const { changeQuantity, itemId, quantity } = this.props;
+    const handleChange = (diff) => {
         changeQuantity(diff, itemId, quantity);
     }
 
-    render() {
-        const { quantity } = this.props;
-        return (
-            <div className="quantity-bar flex">
-                <div id="dec-btn" className="spinner-btn" onClick={() => this.handleChange(-1)}><i className="fas fa-minus"></i></div>
-                <input type="number" value={quantity} readOnly />
-                <div id="inc-btn" className="spinner-btn" onClick={() => this.handleChange(1)}><i className="fas fa-plus"></i></div>
-            </div>
-        )
-    }
+    return (
+        <div className="quantity-bar flex">
+            <div id="dec-btn" className="spinner-btn" onClick={() => handleChange(-1)}><i className="fas fa-minus"></i></div>
+            <input type="number" value={quantity} readOnly />
+            <div id="inc-btn" className="spinner-btn" onClick={() => handleChange(1)}><i className="fas fa-plus"></i></div>
+        </div>
+    )
 }
