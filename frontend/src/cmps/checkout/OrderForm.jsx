52170071193343af23c050a959cfff9be55537ff
talmashiah <talmashiah@gmail.com>
2020-08-18 16:02:03 +0300

    OrderForm turn to hook

--- frontend/src/cmps/checkout/OrderForm.jsx
+++ frontend/src/cmps/checkout/OrderForm.jsx
@@ -1,29 +1,26 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react';
 import Form from '../general/Form.jsx';
 import { Link } from 'react-router-dom';
 
-export default class OrderForm extends Component {
-    state = {
-        inputs: [
-            { type: 'text', name: 'email', label: 'אימייל', validation: ['required', 'email'] },
-            { type: 'text', name: 'fullName', label: 'שם מלא', validation: ['required', 'langAndMin2Char', 'twoWords'] },
-            { type: 'tel', name: 'phone', label: 'טלפון נייד', validation: ['required', 'phone'] },
-            { type: 'text', name: 'city', label: 'עיר / יישוב', validation: ['required', 'langAndMin2Char'] },
-            { type: 'text', name: 'street', label: 'רחוב', validation: ['required'] },
-            { type: 'number', name: 'number', label: 'מספר בית', validation: ['required'] },
-            { type: 'number', name: 'apartment', label: 'דירה' },
-            { type: 'text', name: 'postal', label: 'מיקוד', validation: ['required'] },
-            { type: 'text', name: 'mailbox', label: 'ת.ד' },
-            { type: 'textarea', name: 'notes', label: 'הערות' }
-        ],
-        updatedInputs: null
-    }
+export default function OrderForm({ user, logout, updateForm }) {
 
-    componentDidMount() {
-        const { user } = this.props;
-        const { inputs } = this.state;
+    const [updatedInputs, setUpdatedInputs] = useState(null);
+    const [inputs] = useState([
+        { type: 'text', name: 'email', label: 'אימייל', validation: ['required', 'email'] },
+        { type: 'text', name: 'fullName', label: 'שם מלא', validation: ['required', 'langAndMin2Char', 'twoWords'] },
+        { type: 'tel', name: 'phone', label: 'טלפון נייד', validation: ['required', 'phone'] },
+        { type: 'text', name: 'city', label: 'עיר / יישוב', validation: ['required', 'langAndMin2Char'] },
+        { type: 'text', name: 'street', label: 'רחוב', validation: ['required'] },
+        { type: 'number', name: 'number', label: 'מספר בית', validation: ['required'] },
+        { type: 'number', name: 'apartment', label: 'דירה' },
+        { type: 'text', name: 'postal', label: 'מיקוד', validation: ['required'] },
+        { type: 'text', name: 'mailbox', label: 'ת.ד' },
+        { type: 'textarea', name: 'notes', label: 'הערות' }
+    ]);
+
+    useEffect(() => {
         if (user) {
-            const updatedInputs = inputs.map(input => {
+            const userInputs = inputs.map(input => {
                 let inputCopy = { ...input };
                 for (let [key, value] of Object.entries(user)) {
                     if (inputCopy.name === key) {
@@ -36,37 +33,25 @@ export default class OrderForm extends Component {
                 }
                 return inputCopy;
             })
-            this.setState({ updatedInputs });
+            setUpdatedInputs(userInputs);
         } else {
-            this.setState({ updatedInputs: inputs });
+            setUpdatedInputs(inputs);
         }
-    }
+    }, [user])
 
-    componentDidUpdate(prevProps) {
-        if (!this.props.user && prevProps.user) {
-            this.setState({ updatedInputs: this.state.inputs });
-        }
-    }
-
-    onLogOut = () => {
-        const { logout, updateForm } = this.props;
-        logout()
-        updateForm(false,null);
+    const onLogOut = () => {
+        logout();
+        updateForm(false, null);
     }
 
 
-    render() {
-        const { updatedInputs } = this.state;
-        const { user, updateForm } = this.props;
-        if (!updatedInputs) return null;
-        
-        return (
-            <div className="order-form">
-                <h1>פרטי הזמנה</h1>
-        {user ? <div className="checkout-auth">{`לא ${user.fullName}?`} <span onClick={this.onLogOut}>התנתק</span></div> :
+    if (!updatedInputs) return null;
+    return (
+        <div className="order-form">
+            <h1>פרטי הזמנה</h1>
+            {user ? <div className="checkout-auth">{`לא ${user.fullName}?`} <span onClick={onLogOut}>התנתק</span></div> :
                 <div className="checkout-auth">לקוח/ה רשום? <Link to={{ pathname: 'auth/login', lastRoute: window.location.hash }}>התחבר/י עכשיו</Link></div>}
-                <Form inputs={updatedInputs} updateForm={updateForm} />
-            </div>
-        )
-    }
+            <Form inputs={updatedInputs} updateForm={updateForm} />
+        </div>
+    )
 }
