fadb4e1576742eaa4409feb1d32e43d75d4ae811
talmashiah <talmashiah@gmail.com>
2020-08-18 03:45:05 +0300

    GrowlMessage turn to functional component

--- frontend/src/cmps/general/GrowlMessage.jsx
+++ frontend/src/cmps/general/GrowlMessage.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { connect } from 'react-redux';
 import { setGrowl } from '../../actions/GrowlActions';
@@ -7,28 +7,25 @@ import { ToastContainer, toast } from 'react-toastify';
 import { Flip } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-class GrowlMessage extends Component {
+function GrowlMessage({ growl, setGrowl }) {
 
-    notify = (growl) => {
+    const notify = (growl) => {
         toast[growl.type](growl.msg);
-        this.props.setGrowl(null)
+        setGrowl(null)
     }
 
-    render() {
-        const { growl } = this.props;
-        return (
-            <div>
-                {growl.msg && this.notify(growl)}
-                <ToastContainer
-                    position="top-center"
-                    transition={Flip}
-                    autoClose={2000}
-                    hideProgressBar
-                    newestOnTop
-                    rtl />
-            </div>
-        )
-    }
+    return (
+        <div>
+            {growl.msg && notify(growl)}
+            <ToastContainer
+                position="top-center"
+                transition={Flip}
+                autoClose={2000}
+                hideProgressBar
+                newestOnTop
+                rtl />
+        </div>
+    )
 }
 
 const mapStateToProps = state => {
