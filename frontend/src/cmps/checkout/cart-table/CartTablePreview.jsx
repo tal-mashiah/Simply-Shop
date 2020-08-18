fd8921480d199ab980f55dacf3374d5efb20d16f
talmashiah <talmashiah@gmail.com>
2020-08-18 15:20:49 +0300

    CartTablePreview turn to functional component

--- frontend/src/cmps/checkout/cart-table/CartTablePreview.jsx
+++ frontend/src/cmps/checkout/cart-table/CartTablePreview.jsx
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import QuntityBar from '../../general/QuntityBar.jsx';
 
-export default class CartTablePreview extends Component {
+export default function CartTablePreview({ changeQuantity, deleteItem, item }) {
 
-    onDeleteItem = () => {
-        this.props.deleteItem(this.props.item.product._id);
+    const onDeleteItem = () => {
+        deleteItem(item.product._id);
     }
 
-    render() {
-        const { _id, title, price, imagesUrl } = this.props.item.product;
-        const { item, changeQuantity } = this.props;
-        return (
-            <div className="cart-table-preview flex align-center justify-between">
-                <div className="col-2 flex align-center">
-                    <Link to={`/product/${_id}`}><img src={imagesUrl[0].thumbnail} alt="product img" /></Link>
-                    <div className="title"><Link to={`/product/${_id}`}>{title}</Link></div>
-                </div>
-                <div className="col-3 price">{price}<i className="fas fa-shekel-sign"></i></div>
-                <div className="col-4"><QuntityBar quantity={item.quantity} changeQuantity={changeQuantity} itemId={_id} /></div>
-                <div className="col-5 price">{price * item.quantity}<i className="fas fa-shekel-sign"></i></div>
-                <div className="col-6"><i className="fas fa-trash-alt" onClick={this.onDeleteItem}></i></div>
+    const { _id, title, price, imagesUrl } = item.product;
+    return (
+        <div className="cart-table-preview flex align-center justify-between">
+            <div className="col-2 flex align-center">
+                <Link to={`/product/${_id}`}><img src={imagesUrl[0].thumbnail} alt="product img" /></Link>
+                <div className="title"><Link to={`/product/${_id}`}>{title}</Link></div>
             </div>
-        )
-    }
+            <div className="col-3 price">{price}<i className="fas fa-shekel-sign"></i></div>
+            <div className="col-4"><QuntityBar quantity={item.quantity} changeQuantity={changeQuantity} itemId={_id} /></div>
+            <div className="col-5 price">{price * item.quantity}<i className="fas fa-shekel-sign"></i></div>
+            <div className="col-6"><i className="fas fa-trash-alt" onClick={onDeleteItem}></i></div>
+        </div>
+    )
 }
