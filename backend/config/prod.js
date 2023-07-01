ad0479c0cfcf3f05c5c6a10a6828ea29e99ae401
talmashiah <talmashiah@gmail.com>
2023-07-02 00:32:36 +0300

    asd

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  "dbURL": "mongodb+srv://talmashiah:9090TGTG$@cluster0.5jeer.mongodb.net/corona_db?retryWrites=true&w=majority",
+  "dbURL": "mongodb+srv://talmashiah:9090TGTG$@cluster0.puhsu.mongodb.net/corona_db?retryWrites=true&w=majority",
 }
